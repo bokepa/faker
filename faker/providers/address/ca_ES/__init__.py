f913a9f9462f7acbd256454cc1e45f93b6db02f8
Daniel Hernandez <6077585+bokepa@users.noreply.github.com>
2018-11-02 13:22:09 +0100

    More ca_ES features

--- faker/providers/address/ca_ES/__init__.py
+++ faker/providers/address/ca_ES/__init__.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
 from __future__ import unicode_literals
-from ..es import Provider as AddressProvider
+from ..ca import Provider as AddressProvider
 
 
 class Provider(AddressProvider):
